170de5ab13cbff84be1696eee12877dad1da7e85
Kusainov Yerlan <lagox.mail@gmail.com>
2011-07-02 13:50:32 +0600

    images

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -1,5 +1,7 @@
 class DemoController < ApplicationController
   
+  layout 'admin'
+  
   def index
     #render('hello')
     #redirect_to(:action => 'other_hello')
@@ -14,4 +16,12 @@ class DemoController < ApplicationController
   def other_hello
     render(:text => "Hello Everyone!")
   end
+  
+  def javascript  
+  end
+  
+  def text_helpers
+    
+  end
+  
 end
