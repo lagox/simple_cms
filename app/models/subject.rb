ff66e273001eadc48b8b0213773a345dcd15ee0c
Kusainov Yerlan <lagox.mail@gmail.com>
2011-07-05 19:58:21 +0600

    validations

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -2,8 +2,13 @@ class Subject < ActiveRecord::Base
   
   has_many :pages
 
-  validates :name, :presence => true
-
+  #Don't need to validate(in most cases)
+  # id's, foreign keys, timestamp, booleans, counters
+  validates_presence_of :name
+  validates_length_of :name, :maximum => 255
+  
+  
+  #scope
   scope :visible, where(:visible => true)
   scope :invisible, where(:visible => false)
   scope :search, lambda { |query| where(["name LIKE ?", "%#{query}%"])}
